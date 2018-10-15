1064644a01344bdb7f11a1fcb6ad2efd857878eb
MOJATTO STUDIO <daichiandroid1997@gmail.com>
2018-10-15 10:49:52 +0900

    Initial Commit

---
+++ APP_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  APP_1
+//
+//  Created by MOJATTO STUDIO on 2018/10/15.
+//  Copyright © 2018 MOJATTO STUDIO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
